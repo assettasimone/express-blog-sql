52706d8f30f8b0e86f0f20bba98816b39931db08
assettasimone <assettasimone@gmail.com>
2025-11-06 17:41:44 +0100

    Add environment configuration and update .gitignore
    - Added .env.example for environment variable setup
    - Updated .gitignore to include .env
    - Configured database connection parameters in db.js
    - Modified dev script in package.json to use .env file

--- data/db.js
+++ data/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2')
 
 const connection = mysql.createConnection(
     {
-        host: '',
-        user: '',
-        password: '',
-        database: ''
+        host: 'DB_HOST',
+        user: 'DB_USER',
+        password: 'DB_PASS',
+        database: 'DB_NAME'
     }
 )
