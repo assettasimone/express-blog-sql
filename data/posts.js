03f1ede3f694d86e09589a7820c46c7fe6d00cec
assettasimone <assettasimone@gmail.com>
2025-11-06 17:36:04 +0100

    first commit

---
+++ data/posts.js
@@ -0,0 +1,39 @@
+const blogPosts = [
+    {
+        id: 1,
+        title: 'il mio primo Articolo',
+        content: 'Questo è il contenuto del mio primo articolo',
+        coverImage: 'imgs/anatra1.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        id: 2,
+        title: 'il mio secondo Articolo',
+        content: 'Questo è il contenuto del mio secondo articolo',
+        coverImage: 'imgs/anatra2.jpg',
+        tags: ['tag4', 'tag5', 'tag6']
+    },
+    {
+        id: 3,
+        title: 'il mio terzo Articolo',
+        content: 'Questo è il contenuto del mio terzo articolo',
+        coverImage: 'imgs/anatra3.jpg',
+        tags: ['tag4', 'tag2', 'tag3']
+    },
+    {
+        id: 4,
+        title: 'il mio quarto Articolo',
+        content: 'Questo è il contenuto del mio quarto articolo',
+        coverImage: 'imgs/anatra4.jpg',
+        tags: ['tag8', 'tag7', 'tag3']
+    },
+    {
+        id: 5,
+        title: 'il mio quinto Articolo',
+        content: 'Questo è il contenuto del mio quinto articolo',
+        coverImage: 'imgs/anatra5.jpg',
+        tags: ['tag8', 'tag1', 'tag5']
+    }
+]
+
+module.exports = blogPosts
